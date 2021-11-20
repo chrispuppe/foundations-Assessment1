0afacfcd54b3e4dc46046a02457a9b4d43705273
Chris Puppe <chrispuppe@gmail.com>
2021-11-20 14:14:58 -0800

    password.js extra credit completed

--- password.js
+++ password.js
@@ -11,7 +11,8 @@ const reader = readline.createInterface({
 
 reader.question("Please enter password to be validated: ", function(input){
 	let password = input
-	let hasCapital = false
+    let hasLower = false
+	let hasUpper = false
     let hasNumber = false
     let hasSpecialChar = false
 
@@ -20,7 +21,27 @@ reader.question("Please enter password to be validated: ", function(input){
     } else {
         console.log('Your password is too short')
     } 
+
+    let spChars =/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/
+
+    if (spChars.test(password)){
+        hasSpecialChar = true
+    }
     
+    for (i = 0; i < password.length; i++){
+        if (password[i] === password[i].toLowerCase() && !spChars.test(password) && isNaN(parseInt(password[i]))){
+            hasLower = true
+        }
+        if (password[i] === password[i].toUpperCase()&& !spChars.test(password) && isNaN(parseInt(password[i]))){
+            hasUpper = true
+        }
+        if (!isNaN(parseInt(password[i]))){
+            hasNumber = true
+        }
+        // console.log(parseInt(password[i]))
+    }
+    console.log(hasNumber + "" + hasSpecialChar + "" + hasLower + "" + hasUpper)
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
